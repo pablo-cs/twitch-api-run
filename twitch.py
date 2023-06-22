4842b7f9d5eefb278008272485b2a4b6781f8c72
Pablo Crisostomo Suarez <pcrisostomosuarez5@gmail.com>
2023-06-22 05:34:56 +0000

    Added functionality to project, can search for users and display recent videos and other information

--- twitch.py
+++ twitch.py
@@ -14,3 +14,36 @@ auth_response = requests.post(AUTH_URL, {
 })
 auth_response_data = auth_response.json()
 print(auth_response_data)
+access_token = auth_response_data['access_token']
+
+headers = {'Authorization': 'Bearer {token}'.format(token=access_token),
+          'Client-Id': CLIENT_ID}
+BASE_URL = 'https://api.twitch.tv/helix'
+user_name = ''
+while True:
+  user_name = input('Enter username to search or QUIT: ')
+  if user_name.upper() == 'QUIT':
+    break
+  user_req = requests.get(BASE_URL + '/users?login=' + user_name, headers=headers)
+  user_data = user_req.json()['data']
+  if len(user_data) != 0:
+    user_data = user_req.json()['data'][0]
+    user_id = user_data['id']
+    print("User found!")
+    print("Name:", user_data['display_name'])
+    print("Description:", user_data['description'])
+    print("Broadcaster Type:", user_data['broadcaster_type'])
+    print("Member since:", user_data['created_at'])
+    print("Most recent videos - ")
+    video_req = requests.get(BASE_URL + '/videos?user_id=' + user_id, headers=headers)
+    video_data = video_req.json()['data']
+    i = 0
+    while i < 5 and i < len(video_data):
+      curr_vid = video_data[i]
+      print("Title:", curr_vid['title'])
+      print("Posted:", curr_vid['published_at'])
+      print("Views:", curr_vid['view_count'])
+      i+=1
+  else:
+    print("User not found :(")
+print('Bye!')
